ae28c97af257295a9f6fb3710b2dc091deb6fe9d
Caroline Oliveira <anacdolima@gmail.com>
2023-09-21 13:02:43 -0300

    feat: create header menu

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameCardComponent } from '../components/game-card/game-card.component';
 import { MainPageComponent } from './main/main-page.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 @NgModule({
-  declarations: [GameCardComponent, MainPageComponent],
+  declarations: [GameCardComponent, HeaderComponent, MainPageComponent],
   imports: [CommonModule],
   exports: [MainPageComponent],
 })
